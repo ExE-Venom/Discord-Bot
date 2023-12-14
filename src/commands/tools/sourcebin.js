ecebb9dcfc58149a972a7e4e661727e79ee0f953
AlmaziikDev <almazgamer228@gmail.com>
2023-12-14 14:29:24 +0300

    Fixed `sourcebin`

--- src/commands/tools/sourcebin.js
+++ src/commands/tools/sourcebin.js
@@ -4,18 +4,18 @@ const sourcebin = require('sourcebin');
 module.exports = async (client, interaction, args) => {
     const language = interaction.options.getString('language');
 
-    await interaction.editReply('Please enter your code within the next 60 seconds.');
+    const initialMessage = await interaction.editReply('Please enter your code within the next 3 minutes.');
 
-    const filter = (msg) => msg.author.id === interaction.user.id; 
+    const filter = (msg) => msg.author.id === interaction.user.id;
     const collector = interaction.channel.createMessageCollector({
         filter,
-        time: 60000, 
-        max: 1, 
+        time: 180000,
+        max: 1,
     });
 
     collector.on('collect', async (msg) => {
         const code = msg.content;
-        await msg.delete(); 
+        await msg.delete();
 
         const bin = await sourcebin.create(
             [
@@ -25,27 +25,30 @@ module.exports = async (client, interaction, args) => {
                 },
             ],
             {
-                title: '💻・Random Code',
+                title: '💻・Code',
                 description: 'This code was uploaded via Almaz (Discord Bot).',
             }
         );
 
-        client.succNormal({
-            text: `Your code has been posted!`,
-            fields: [
-                {
-                    name: `🔗┇Link`,
-                    value: `[Click here to see your code](${bin.url})`,
-                    inline: true,
-                }
-            ],
-            type: 'editreply'
-        }, interaction);
+        const successEmbed = new Discord.EmbedBuilder()
+        .setColor('#57F287')
+        .setTitle('💻 Your code has been posted!')
+        .setDescription(`[Click here to see your code](${bin.url})`)
+        .setTimestamp();
+    
+    const successMessage = await interaction.followUp({
+        embeds: [successEmbed]
+    });    
+
+        setTimeout(() => {
+            initialMessage.delete().catch(console.error); // Delete the initial message after 10 seconds
+        }, 5000);
     });
 
     collector.on('end', (collected, reason) => {
         if (reason === 'time') {
             interaction.followUp('Time expired, please try again.');
+            initialMessage.delete().catch(console.error); // Delete the initial message if time expires
         }
     });
 };
